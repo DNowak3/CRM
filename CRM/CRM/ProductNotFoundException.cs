54e3c44d5a3de88fb0d70d15ab22dfd2495b53f7
mastek@student.agh.edu.pl <mastek@student.agh.edu.pl>
2020-12-16 01:30:21 +0100

    Klasy Produkt, Umowa i interfejs do klasy Umowa

---
+++ CRM/CRM/ProductNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM
+{
+    class ProductNotFoundException :Exception
+    {
+        public ProductNotFoundException() : base() { }
+        public ProductNotFoundException(string komunikat) : base(komunikat) { }
+    }
+}
