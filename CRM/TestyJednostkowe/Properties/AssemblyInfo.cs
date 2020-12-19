3ed5ae333016112c78d819ec87c15969232d558d
Hubert Kycia <kyciahubert@studentaghedupl.onmicrosoft.com>
2020-12-19 14:27:21 +0100

    Utworzenie projektu TestyJednostkowe

---
+++ CRM/TestyJednostkowe/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestyJednostkowe")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestyJednostkowe")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("27d660bd-7801-467e-8521-b1abf2e1d210")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
