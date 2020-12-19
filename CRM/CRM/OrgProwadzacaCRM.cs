9567f61857c1d0e976efa49a30a8bdb0106405dc
DNowak3 <danowak@student.agh.edu.pl>
2020-12-20 00:47:35 +0100

    Część dokumentacji klasy OrgProwadzacaCRM
    + baza do testów jednostkowych tej klasy

--- CRM/CRM/OrgProwadzacaCRM.cs
+++ CRM/CRM/OrgProwadzacaCRM.cs
@@ -9,12 +9,26 @@ using System.Xml.Serialization;
 namespace CRM
 {
     [Serializable]
-    
+    /// <summary>
+    /// Klasa definiująca organizację, która prowadzi dla siebie system CRM.
+    /// </summary>
     public class OrgProwadzacaCRM:Organizacja,IZapisywalna
     {
+        /// <summary>
+        /// Lista zawierająca wszystkich pracowników organizacji.
+        /// </summary>
         List<Pracownik> _listaPracownikow;
+        /// <summary>
+        /// Lista zawierająca wszystkie produkty, które sprzedaje organizacja.
+        /// </summary>
         List<Produkt> _listaProduktow;
+        /// <summary>
+        /// Lista zawierająca wszystkich konkurentów organizacji.
+        /// </summary>
         List<Konkurent> _listaKonkurentow;
+        /// <summary>
+        /// Lista zawierająca wszystkich klientów organizacji.
+        /// </summary>
         List<Klient> _listaKlientow;
 
         public List<Pracownik> ListaPracownikow { get => _listaPracownikow;}
@@ -22,6 +36,7 @@ namespace CRM
         public List<Konkurent> ListaKonkurentow { get => _listaKonkurentow; }
         public List<Klient> ListaKlientow { get => _listaKlientow;}
 
+        #region Konstruktory
         public OrgProwadzacaCRM()
         {
             _listaPracownikow = new List<Pracownik>();
@@ -45,7 +60,12 @@ namespace CRM
             _listaKlientow = new List<Klient>();
             _listaKonkurentow = new List<Konkurent>();
         }
+        #endregion
         #region Funkcje Pracownicy
+        /// <summary>
+        /// Funkcja dodająca obiekt Pracownik do listy pracowników organizacji. Powoduje wyrzucenie wyjątku AlreadyInListException(), jeżeli taki obiekt już znajduje się na liście.
+        /// </summary>
+        /// <param name="p">Obiekt Pracownik</param>
         public void DodajPracownika(Pracownik p)
         {
             if (JestPracownikiem(p))
@@ -54,6 +74,14 @@ namespace CRM
             }
             _listaPracownikow.Add(p);
         }
+        /// <summary>
+        /// Funkcja usuwając pracownika z listy pracowników organizacji.
+        /// </summary>
+        /// <param name="imie">Imie pracownika do usunięcia.</param>
+        /// <param name="nazwisko">Nazwisko pracownika do usunięcia.</param>
+        /// <param name="stanowisko">Stanowisko pracownika do usunięcia</param>
+        /// <returns>Prawdę, jeśli taki pracownik był na liście i go usunięto,
+        /// Fałsz, jeśli takiego pracownika nie było.</returns>
         public bool UsunPracownika(string imie, string nazwisko, Stanowiska stanowisko)
         {
             if (_listaPracownikow.Count() != 0 && JestPracownikiem(imie, nazwisko, stanowisko))
@@ -63,6 +91,12 @@ namespace CRM
             }
             return false;
         }
+        /// <summary>
+        /// Funkcja usuwając pracownika z listy pracowników organizacji.
+        /// </summary>
+        /// <param name="p">Obiekt Pracownik przeznaczony do usunięcia.</param>
+        /// <returns>Prawdę, jeśli taki pracownik był na liście i go usunięto,
+        /// Fałsz, jeśli takiego pracownika nie było.</returns>
         public bool UsunPracownika(Pracownik p)
         {
             if (_listaPracownikow.Count() != 0 && JestPracownikiem(p))
@@ -72,10 +106,17 @@ namespace CRM
             }
             return false;
         }
+        /// <summary>
+        /// Funkcja usuwa wszystkich pracowników z listy.
+        /// </summary>
         public void UsunWszystkichPracownikow()
         {
             _listaPracownikow.Clear();
         }
+        /// <summary>
+        /// Podaje liczbę pracowników pracujących w organizacji.
+        /// </summary>
+        /// <returns>Zwraca liczbę pracowników.</returns>
         public int PodajIloscPracownikow()
         {
             if(_listaPracownikow is null)
@@ -84,22 +125,51 @@ namespace CRM
             }
             return _listaPracownikow.Count();
         }
+        /// <summary>
+        /// Funkcja sprawdza, czy podany obiekt Pracownik znajduje się na liście pracowników.
+        /// </summary>
+        /// <param name="p">Obiekt pracownik, którego szukamy na liście pracowników.</param>
+        /// <returns>Prawdę, jeśli obiekt Pracownik jest pracownikiem oraganizacji prowadzącej CRM,
+        /// Fałsz, jeśli nie jest.</returns>
         public bool JestPracownikiem(Pracownik p)
         {
             return _listaPracownikow.Contains(p);
         }
+        /// <summary>
+        /// Funkcja sprawdza, czy pracownik o podanym imeiniu, nazwisku i stanowisku znajduje się na liście pracowników.
+        /// </summary>
+        /// <param name="imie">Imie pracownika, który jest szukany.</param>
+        /// <param name="nazwisko">Nazwisko pracownika, który jest szukany.</param>
+        /// <param name="stanowisko">Stanowisko pracownika, który jest szukany.</param>
+        /// <returns>Prawdę, jeśli obiekt o takich parametrach jest pracownikiem oraganizacji prowadzącej CRM,
+        /// Fałsz, jeśli nie jest.</returns>
         public bool JestPracownikiem(string imie, string nazwisko, Stanowiska stanowisko)
         {
             return _listaPracownikow.Exists(p => p.Imie == imie && p.Nazwisko == nazwisko && p.Stanowisko == stanowisko);
         }
+        /// <summary>
+        /// Funkcja wyszukuje wszystkich pracowników organizacji prowadzącej CRM znajdujących się na podanym stanowisku.
+        /// </summary>
+        /// <param name="stanowisko">Stanowisko, na którym szukamy pracowników.</param>
+        /// <returns>Listę pracowników, znajdujących się na podanym stanowisku.</returns>
         public List<Pracownik> ZnajdzWszystkichPracownikowStanowisko(Stanowiska stanowisko)
         {
             return _listaPracownikow.FindAll(p => p.Stanowisko == stanowisko);
         }
+        /// <summary>
+        /// Funkcja wyszukuje wszystkich pracowników organizacji prowadzącej CRM o podane płci.
+        /// </summary>
+        /// <param name="plec">Płeć, jaką mają szukani pracownicy.</param>
+        /// <returns>Listę pracowników, o podanej płci.</returns>
         public List<Pracownik> ZnajdzWszystkichPracownikowPlec(Plcie plec)
         {
             return _listaPracownikow.FindAll(p => p.Plec == plec);
         }
+        /// <summary>
+        /// Funkcja wyszukuje wszystkich pracowników organizacji prowadzącej CRM, którzy zaczeli w niej pracować przed podaną datą.
+        /// </summary>
+        /// <param name="dataRozpoczecia">Data, przed którą zostali zatrudnieni szukani pracownicy.</param>
+        /// <returns>Listę pracowników, którzy zostali zatrudnieni przed podaną datą.</returns>
         public List<Pracownik> ZnajdzWszystkichPracownikowPracaPrzed(string dataRozpoczecia)
         {
             DateTime DataRozpoczecia;
@@ -107,13 +177,37 @@ namespace CRM
 
             return _listaPracownikow.FindAll(p => p.DataRozpoczeciaPracy < DataRozpoczecia);
         }
+        /// <summary>
+        /// Funkcja sortuje wszystkich pracowników alfabetycznie, najpierw po nazwisku, potem po imieniu.
+        /// </summary>
         public void PracownicySortujAlfabetycznie()
         {
             _listaPracownikow.Sort();
         }
+        /// <summary>
+        /// Funkcja sortuje wszystkich pracowników po dacie zatrudnienia. 
+        /// </summary>
+        /// <param name="rosnaco">True oznacza sortowanie od najwcześniejszego zatrudnienia do najpóźniejszego,
+        /// False oznacza sortowanie od najpóźniejszego zatrudnienia do najwcześniejszego.</param>
+        public void PracownicySortujPoDacieZatrudnienia(bool rosnaco=true)
+        {
+            if (rosnaco)
+            {
+                _listaPracownikow.Sort((x, y) => x.DataRozpoczeciaPracy.CompareTo(y.DataRozpoczeciaPracy));
+            }
+            else
+            {
+                _listaPracownikow.Sort((x, y) => y.DataRozpoczeciaPracy.CompareTo(x.DataRozpoczeciaPracy));
+
+            }
+        }
         #endregion
 
         #region Funkcje Produkty
+        /// <summary>
+        /// Funkcja dodająca obiekt Produktów do listy produktów jakie oferuje organizacja. Powoduje wyrzucenie wyjątku AlreadyInListException(), jeżeli taki obiekt już znajduje się na liście.
+        /// </summary>
+        /// <param name="p">Obiekt Produkt</param>
         public void DodajProdukt(Produkt p)
         {
             if (JestProduktem(p))
@@ -122,6 +216,12 @@ namespace CRM
             }
             _listaProduktow.Add(p);
         }
+        /// <summary>
+        /// Funkcja usuwając produkt z listy produktów, jakie oferuje organizacja.
+        /// </summary>
+        /// <param name="kod">Kod produktu do usunięcia.</param>
+        /// <returns>Prawdę, jeśli taki produkt był na liście i go usunięto,
+        /// Fałsz, jeśli takiego produktu nie było.</returns>
         public bool UsunProdukt(string kod)
         {
             if (_listaProduktow.Count() != 0 && JestProduktem(kod))
@@ -131,6 +231,12 @@ namespace CRM
             }
             return false;
         }
+        /// <summary>
+        /// Funkcja usuwając produkt z listy produktów, jakie oferuje organizacja.
+        /// </summary>
+        /// <param name="p">Obiekt Produkt do usunięcia.</param>
+        /// <returns>Prawdę, jeśli taki produkt był na liście i go usunięto,
+        /// Fałsz, jeśli takiego produktu nie było.</returns>
         public bool UsunProdukt(Produkt p)
         {
             if (_listaProduktow.Count() != 0 && JestProduktem(p))
@@ -140,10 +246,17 @@ namespace CRM
             }
             return false;
         }
+        /// <summary>
+        /// Funkcja usuwa wszystkie produkty z listy produktów.
+        /// </summary>
         public void UsunWszystkieProdukty()
         {
             _listaProduktow.Clear();
         }
+        /// <summary>
+        /// Funkcja oblicza liczbę produktów oferowanych przez organizację.
+        /// </summary>
+        /// <returns>Zwraca liczbę produktów.</returns>
         public int PodajIloscProduktow()
         {
             if (_listaPracownikow is null)
@@ -152,15 +265,33 @@ namespace CRM
             }
             return _listaProduktow.Count();
         }
+        /// <summary>
+        /// Funkcja sprawdza, czy podany obiekt Produkt znajduje się na liście produktów organizacji.
+        /// </summary>
+        /// <param name="p">Obiekt produkt, którego szukamy na liście produktów.</param>
+        /// <returns>Prawdę, jeśli obiekt Produkt jest produktem oferowanym przez organizację prowadzącą CRM,
+        /// Fałsz, jeśli nie jest.</returns>
         public bool JestProduktem(Produkt p)
         {
             return _listaProduktow.Contains(p);
         }
+        /// <summary>
+        /// Funkcja sprawdza, czy produkt o podanym kodzie znajduje się na liście produktó organizacji.
+        /// </summary>
+        /// <param name="kod">Kod produktu, który jest szukany.</param>
+        /// <returns>Prawdę, jeśli obiekt o takim kodzie jest produktem oferowanym przez oraganizację prowadzącą CRM,
+        /// Fałsz, jeśli nie jest.</returns>
         public bool JestProduktem(string kod)
         {
             return _listaProduktow.Exists(p => p.Kod == kod);
         }
-        public List<Produkt> ZnajdzWszystkieProduktyCena(float porownywalnaCena, bool tansze = false)
+        /// <summary>
+        /// Funkcja wyszukuje wszystkie produkty oferowane przez organizację prowadzącą CRM poniżej lub powyżej podanej ceny.
+        /// </summary>
+        /// <param name="porownywalnaCena">Cena wobec której porównywane są produkty.</param>
+        /// <param name="tansze">Parametr służący do opisania, czy chcemy sobaczyć produkty tańsze czy droższe. True oznacza, że chcemy uzyskać produkty tańsze (co też jest domyślną wartością), a false, że produkty droższe od podanej ceny.</param>
+        /// <returns>LListę produktów tańszych/droższych od podanej ceny.</returns>
+        public List<Produkt> ZnajdzWszystkieProduktyCena(float porownywalnaCena, bool tansze = true)
         {
             if (tansze)
             {
@@ -171,10 +302,19 @@ namespace CRM
                 return _listaProduktow.FindAll(p => p.Cena > porownywalnaCena);
             }
         }
+        /// <summary>
+        /// Funkcja wyszukuje wszystkie pprodukty oferowane przez organizację prowadzącą CRM, które sprzedawane są w podanej jednostce.
+        /// </summary>
+        /// <param name="jednostka">Jednostka, w jakiej sprzedawane są produkty, których szukamy.</param>
+        /// <returns>Listę produktów o danej jednostce.</returns>
         public List<Produkt> ZnajdzWszystkieProduktyJednostka(Jednostki jednostka)
         {
             return _listaProduktow.FindAll(p => p.Jednostka == jednostka);
         }
+        /// <summary>
+        /// Funkcja sortująca produkty oferowane przez organizację prowadzącą CRM według ceny. 
+        /// </summary>
+        /// <param name="rosnaco">True, produkty sortowane są rosnąco wg ceny, False, produkty sortowane są malejąco wg ceny.</param>
         public void ProduktySortujPoCenie(bool rosnaco=true)
         {
             if (rosnaco)
@@ -187,6 +327,9 @@ namespace CRM
 
             }
         }
+        /// <summary>
+        /// Funkcja sortuje wszystkich produkty alfabetycznie po ich nazwie.
+        /// </summary>
         public void ProduktySortujPoNazwie()
         {
             _listaProduktow.Sort();
---
+++ CRM/TestyJednostkowe/TestyOrgProwadzacaCRM.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using CRM;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestyJednostkowe
+{
+    [TestClass]
+    public class TestyOrgProwadzacaCRM
+    {
+        [TestMethod]
+        public void DodawaniePracownikow()
+        {
+            OrgProwadzacaCRM temp = new OrgProwadzacaCRM();
+            int licznik = 1;
+            temp.DodajPracownika(new Pracownik());
+            Assert.AreEqual(licznik, temp.PodajIloscPracownikow());
+        }
+        [TestMethod]
+        public void DodawanieProduktow()
+        {
+            OrgProwadzacaCRM temp = new OrgProwadzacaCRM();
+            int licznik = 1;
+            temp.DodajProdukt(new Produkt());
+            Assert.AreEqual(licznik, temp.PodajIloscProduktow());
+        }
+        [TestMethod]
+        public void DodawanieKonkurentow()
+        {
+            OrgProwadzacaCRM temp = new OrgProwadzacaCRM();
+            int licznik = 1;
+            temp.DodajKonkurenta(new Konkurent());
+            Assert.AreEqual(licznik, temp.PodajIloscKonkurentow());
+        }
+        [TestMethod]
+        public void DodawanieKlientow()
+        {
+            OrgProwadzacaCRM temp = new OrgProwadzacaCRM();
+            int licznik = 1;
+            temp.DodajKlienta(new Klient());
+            Assert.AreEqual(licznik, temp.PodajIloscKlientow());
+        }
+    }
+}
