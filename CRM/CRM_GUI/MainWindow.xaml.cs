1b386e0a781aa551fc4e7d54090557ee8db929c8
DNowak3 <danowak@student.agh.edu.pl>
2021-01-04 16:36:28 +0100

    .

--- CRM/CRM_GUI/MainWindow.xaml.cs
+++ CRM/CRM_GUI/MainWindow.xaml.cs
@@ -80,7 +80,7 @@ namespace CRM_GUI
                     {
                         _orgCRM.ZapiszJSON(nazwaPliku);
                     }
-                    _orgZPliku = (OrgProwadzacaCRM)_orgCRM.Clone();
+                    //_orgZPliku = (OrgProwadzacaCRM)_orgCRM.Clone();
                 }
                 catch (Exception)
                 {
@@ -122,7 +122,7 @@ namespace CRM_GUI
                     txtNIP.Text = _orgCRM.Nip;
                     txtAdres.Text = _orgCRM.Adres;
 
-                   _orgZPliku = (OrgProwadzacaCRM)_orgCRM.Clone();
+                   //_orgZPliku = (OrgProwadzacaCRM)_orgCRM.Clone();
                 }
             }
         }
